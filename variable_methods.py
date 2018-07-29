27f5d64b4a404581d06c102305e37f8baa594470
Tieshia Francis <francistie@gmail.com>
2018-07-29 05:07:29 +0000

    Initial commit: created variable_methods.py

---
+++ variable_methods.py
@@ -0,0 +1,17 @@
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10
+
+string_variable = "hello"
+single_quotes = "strings can have single quotes"
+
+# print(my_variable)
+# print(string_variable)
+
+##################################### METHODS #################################
+
+def my_print_method(my_parameter):
+    print(my_parameter)
+
+my_print_method()
